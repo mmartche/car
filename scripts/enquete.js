7bc38599b69e99f404cd0c59cc00218ada4508c7
martche <mmartche@hotmail.com>
2013-09-30 21:05:46 -0300

    m

---
+++ scripts/enquete.js
@@ -0,0 +1,13 @@
+function votaEnquete(){
+	console.log("start vote");
+	$("#homeEnquete .fieldsEnquete").animate({"width": "0"}, "slow");
+	/*
+	$("#homeEnquete .resultEnquete1 span").text("42");
+	$("#homeEnquete .resultEnquete1").animate({"width": "42px"}, "fast");
+	$("#homeEnquete .resultEnquete2 span").text("1");
+	$("#homeEnquete .resultEnquete2").animate({"width": "1px"}, "fast");
+	$("#homeEnquete .resultEnquete3 span").text("121000993");
+	$("#homeEnquete .resultEnquete3").animate({"width": "121000993"}, "fast"); 
+	*/
+	return false;
+}
