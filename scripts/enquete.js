7e7c514b38f43612468847e1d20c5b5fc1fee7a5
martche <mmartche@hotmail.com>
2013-09-30 21:49:25 -0300

    m

--- scripts/enquete.js
+++ scripts/enquete.js
@@ -1,13 +1,24 @@
 function votaEnquete(){
 	console.log("start vote");
-	$("#homeEnquete .fieldsEnquete").animate({"width": "0"}, "slow");
-	/*
-	$("#homeEnquete .resultEnquete1 span").text("42");
-	$("#homeEnquete .resultEnquete1").animate({"width": "42px"}, "fast");
-	$("#homeEnquete .resultEnquete2 span").text("1");
-	$("#homeEnquete .resultEnquete2").animate({"width": "1px"}, "fast");
-	$("#homeEnquete .resultEnquete3 span").text("121000993");
-	$("#homeEnquete .resultEnquete3").animate({"width": "121000993"}, "fast"); 
-	*/
+	//check and update values from survey then insert and anime
+	$("#homeEnquete .fieldsEnquete").animate({"width": "0"}, 500,'linear', function() {
+		$("#homeEnquete .resultEnquete1 .spanResultEnquete").text("42");
+		$("#homeEnquete .resultEnquete1").animate({"width": "42px"}, "fast");
+		$("#homeEnquete .resultEnquete2 .spanResultEnquete").text("1");
+		$("#homeEnquete .resultEnquete2").animate({"width": "1px"}, "fast");
+		$("#homeEnquete .resultEnquete3 .spanResultEnquete").text("121000993");
+		$("#homeEnquete .resultEnquete3").animate({"width": "121000993"}, "fast"); 
+  	});
+	
+	//change vote btn properties
 	return false;
+}
+function resetEnquete() {
+	$("#homeEnquete .fieldsEnquete").animate({"width": "195px"}, "fast");
+	$("#homeEnquete .resultEnquete1 span").text("");
+	$("#homeEnquete .resultEnquete1").animate({"width": "0px"}, "fast");
+	$("#homeEnquete .resultEnquete2 span").text("");
+	$("#homeEnquete .resultEnquete2").animate({"width": "0px"}, "fast");
+	$("#homeEnquete .resultEnquete3 span").text("");
+	$("#homeEnquete .resultEnquete3").animate({"width": "0px"}, "fast"); 
 }
